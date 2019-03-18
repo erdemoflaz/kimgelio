40a5b42f4a531d41cfd921394e538bbafef22142
Erdem Oflaz <erdemoflaz4@gmail.com>
2019-03-18 22:55:27 +0300

    ✨  events pages edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   has_many :invitation, dependent: :delete_all
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name_surname, presence: true
 end
