940ad3dee7109c34f7ba566996e1b97b7cb703a9
mesabuca <msbuyukcaglayan@gmail.com>
2019-03-19 02:36:07 +0300

    Profiles declined events

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:id])
     @events = @user.events
     @acceptedEvents = Event.where(id: @user.invitations.accepted.collect(&:event_id))
+    @declinedEvents = Event.where(id: @user.invitations.declined.collect(&:event_id))
   end
 end
