2572a9dc54fb05622d697ffd2d9b8a5aad29fcb9
mesabuca <msbuyukcaglayan@gmail.com>
2019-03-18 13:12:02 +0300

    Code refactor

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,37 +1,42 @@
 class EventsController < ApplicationController
+  before_action :set_events, only: %i[show edit update destroy]
+
   def index
     @events = Event.all
   end
+
   def new
     @event = Event.new
   end
+
   def create
     @event = current_user.events.new(event_params)
     if @event.save
       redirect_to event_path(@event)
     end
   end
-  def show
-    @event = Event.find(params[:id])
-  end
-  def edit
-    @event = Event.find(params[:id])
-  end
+
+  def show; end
+
+  def edit; end
 
   def update
-    @event = Event.find(params[:id])
     if @event.update(event_params)
       redirect_to event_path(@event)
     end
   end
+
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
-    redirect_to evebts_url, notice: 'SİLİNDİ'
+    redirect_to events_url, notice: 'SİLİNDİ'
   end
 
   private
   def event_params
     params.require(:event).permit(:title, :description, :date, :location, :limit)
+  end
+
+  def set_events
+    @event = Event.find(params[:id])
   end
 end
