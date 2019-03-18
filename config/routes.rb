c77b799fca49949af14250c42e4f45b83aa04d3f
mesabuca <msbuyukcaglayan@gmail.com>
2019-03-18 23:40:53 +0300

    Accept Decline priviliges setted

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
 
   root to: 'welcomes#index'
 
-  resources :events
+  resources :events do
+    resources :invitations
+    get 'invitations/:id/accept', to: 'invitations#accept', as: 'accept_invite'
+    get 'invitations/:id/decline', to: 'invitations#decline', as: 'decline_invite'
+  end
 
-  resources :invitations
 
-  get 'invitations/:id/accept', to: 'invitations#accept', as: 'accept_invite'
-  get 'invitations/:id/decline', to: 'invitations#decline', as: 'decline_invite'
 
   # post '/events/:id', to: 'invitations/create', as: :create_invitations
 end
