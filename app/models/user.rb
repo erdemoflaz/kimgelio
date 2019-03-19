b0a10947df08c42aa71cb8324e26ed3ce9432ef1
Erdem Oflaz <erdemoflaz4@gmail.com>
2019-03-19 04:37:08 +0300

    🔧  pushed last changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :events, dependent: :destroy
-  has_many :invitations, dependent: :delete_all
+  has_many :invitations
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
